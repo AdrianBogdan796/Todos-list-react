712feec2ec5ef48c69951c51de8d505bd33fb604
Adrian Bogdan <enu999@o2.pl>
2023-04-27 17:24:32 +0200

    localStorege added

--- src/App.js
+++ src/App.js
@@ -5,53 +5,17 @@ import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
 import { useState } from "react";
+import { useTasks } from "./useTasks";
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([
-    { id: 1, content: "nauczyć się czego kolowiek na kursie", done: false },
-    { id: 2, content: "znaleźć sobie coś do roboty", done: true },
-  ]);
 
   const toggleHideDone = () => {
     setHideDone((hideDone) => !hideDone);
   };
 
-  const removeTask = (id) => {
-    setTasks((tasks) => tasks.filter((task) => task.id !== id));
-  };
-
-  const toggleTaskDone = (id) => {
-    setTasks((tasks) =>
-      tasks.map((task) => {
-        if (task.id === id) {
-          return { ...task, done: !task.done };
-        }
-
-        return task;
-      })
-    );
-  };
-
-  const setAllDone = () => {
-    setTasks((tasks) =>
-      tasks.map((task) => ({
-        ...task,
-        done: true,
-      }))
-    );
-  };
-
-  const addNewTask = (content) => {
-    setTasks((tasks) => [
-      ...tasks,
-      {
-        content,
-        done: false,
-        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
-      },
-    ]);
-  };
+  const { tasks, removeTask, toggleTaskDone, setAllDone, addNewTask } =
+    useTasks();
 
   return (
     <Container>
